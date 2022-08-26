be13de62fcce82034af092211e326d4c336669ce
Riyadh Uddin <noreplythnx@gmail.com>
2022-08-26 23:30:16 +0600

    amplify bk

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
-
+import Amplify from '@aws-amplify';
+import awsconfig from '../aws-exports';
+Amplify.configure(awsconfig);
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
