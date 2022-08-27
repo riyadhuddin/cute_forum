e859d4c2ec6a2e81c83ba22cd74b1dece618a257
Riyadh Uddin <noreplythnx@gmail.com>
2022-08-27 11:02:56 +0600

    hmm

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
-import Amplify from '@aws-amplify';
-import awsconfig from '../aws-exports';
-Amplify.configure(awsconfig);
+import Amplify from "aws-amplify";
+import config from "../src/aws-exports";
+Amplify.configure(config);
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
