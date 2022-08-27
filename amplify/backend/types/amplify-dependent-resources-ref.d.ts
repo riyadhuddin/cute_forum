fd2b5cda7deb3f5edc942cb982d1ceb1d4865fc6
Riyadh Uddin <noreplythnx@gmail.com>
2022-08-27 11:43:55 +0600

    end

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3412a5a76": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
